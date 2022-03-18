4675af6765d878c53ca38cbb1a4b44cea6bf8f2c
KMIRKOMIL <96414340+KMIRKOMIL@users.noreply.github.com>
2022-03-18 23:02:49 +0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,7 @@
 let qoy = +prompt('Qo`ylar sonini kiriting')
+while (isNaN(qoy) || qoy <= 0) {
+    qoy = +prompt('Son kiriting baraka topkur!!!')
+}
 let total = ''
 for (let i = 0; i < qoy; i++) {
     if (i == 0) {
