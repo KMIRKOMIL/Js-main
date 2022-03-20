927be9908730fb155a96af223f22ac28b0f18a60
KMIRKOMIL <96414340+KMIRKOMIL@users.noreply.github.com>
2022-03-20 09:10:26 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -25,7 +25,7 @@ if (answer % 2 == 0) {
 for (let i = 0; i < answer; i++) {
     for (let j = 0; j < answer; j++) {
         let end = answer - 1
-        if (j == i || i == 0 || j == 0 || j == answer - 1 || i == answer - 1 || j == end - i) {
+        if (j == i || i == 0 || j == 0 || j == end || i == end || j == end - i) {
             total += "[X]"
         }
         else {
